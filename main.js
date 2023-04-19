3db3307170da1256227f6570182298cfd289e2ff
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2023-04-19 13:14:40 -0400

    Check all rows.  Delete empty rows.

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 // Checks for completed entries on entry page & adds them to Calendar Page and Gcal.
 function processNewEntries(){
-  var [entries, rowNumber] = getEnteredData()
+  var entries = getEnteredData()
+  entries.shift() // Remove heading line
+  rowNumber = 2 // Start on the row after the headings
 
   for (const row of entries) {
     var entryDict = Object.fromEntries(InputPage['headingRange'].map((key, i) => [key, row[i]]));  // Pair headings with values in current row
+
+    // Try updating calendar with new entry data
+    // Delete row if successful otherwise move to next row
     if (entryDict[InputPage['checkboxName']]){
-      updateSheets(entryDict, rowNumber)
+      if (updateSheets(entryDict, rowNumber)){
+        InputSheet.deleteRow(rowNumber)
+      }
+      else {
+        rowNumber ++
+      }
       updateGcal(entryDict)
     }
-    rowNumber ++
+  
+    // Delete empty rows OR move to the next row.
+    else if (Object.values(entryDict).every(value => !value)){
+      InputSheet.deleteRow(rowNumber)
+    }
+    else {
+      rowNumber ++
+    }
   }
   sortSheet()
 };
@@ -17,12 +34,22 @@ function processNewEntries(){
 // Helper Functions
 // ###################
 
+// Attempt to data on current row to Google Sheets or GCal
+// Sheet and Gcal functions perform validation checks.
+function updateEvents(entryDict, rowNumber){
+  if (entryDict[InputPage['checkboxName']]){
+    if (updateSheets(entryDict, rowNumber)){
+      InputSheet.deleteRow(rowNumber)
+    }
+    updateGcal(entryDict)
+  }
+};
+
 // Returns a list of all entries on entry page and the row they start on.
 function getEnteredData() {
-  const range = InputSheet.getRange(InputPage['inputRange'])
+  const range = InputSheet.getDataRange()
   var entries = range.getValues()
-  var rowNumber = range.getRow()
-  return [entries, rowNumber]
+  return entries
 };
 
 // Sorts dates in the proper order & ensures groupings are retained correctly.
