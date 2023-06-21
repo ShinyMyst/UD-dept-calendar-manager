96774d87e9656ac995536c9359721b0226f62520
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2023-06-21 11:43:07 -0400

    sheets.js reorganized.
    Removed uneeded variables
    addEvent takes place of process events
    getSheetData now using references instead of hardcode
    added explanations
    
    Still need to do some reorganization.

--- sheets.js
+++ sheets.js
@@ -1,25 +1,28 @@
 // ###################
 // Entry Class
 // ###################
+
+//TODO - Try adding a create Sorting ID function.
+// This could simplify later function and allow date checking for duplicates
+
 class SheetEntry {
   constructor() {
-    this.entryDict = null;  // <-- REMOVE WHEN POSSIBLE TODO
-    this.sheetData = getSheetData()   // All events on page
-    this.eventDict = null             // Dict with current event
+    this.sheetData = getSheetData()  
   };
 
-
-
-  // Update the current event dict
   // Returns true if sheet data is valid
   validEvent(eventData){
-    this.eventDict = createEventDict(eventData)
+    console.log("SHEETDATA", this.sheetData)
+    console.log("EVENTDATA", eventData)
+
 
     switch (true) {
       // Duplicate Entry
+      /*
       case (matchEventData(eventData, this.sheetData)):
         console.log("DUPLICATE FOUND")
         return false
+      */
 
       // Add other validation here
 
@@ -28,45 +31,52 @@ class SheetEntry {
     }
   };
 
-  // Writes the data for current row onto last row of sheet
-  writeRow(){
-    // TODO this is essentially the process function.
-    // Ideally, stop the start and end dates from being called 30 times.
-    // perhaps add a class reference at the top?
-  };
+  // Add event data to last row of sheet and to sheetData
+  addEvent(eventData){
+    // Variables for readability
+    const startDate = eventData[HEADING['StartDate']]
+    const endDate = eventData[HEADING['EndDate']]
 
-  _processInput(){
-    this.calendarDict[HEADING['EventID']] = ''
-    const START = this.entryDict[HEADING['StartDate']]
-    const END = this.entryDict[HEADING['EndDate']]
-    if (this.entryDict[HEADING['EndDate']].equals("")){
-      END = START
-    }
-
+    // Write Entry
     switch (true) {
       // Singular Month
-      case MONTHS.includes(START):
-        this._writeSingularMonth(START);
-        break;
-    
+      case MONTHS.includes(startDate):
+        this._writeSingularMonth(startDate);
+        break;   
       // Singular Date
-      case START.getTime() == END.getTime():
-        this._writeSingularDate(START);
+      case endDate === '':
+        this._writeSingularDate(startDate);
         break;
-    
+      case startDate.getTime() == endDate:
+        this._writeSingularDate(startDate);
+        break;
       // Date Range
       default:
-        this._writeDateRange(START, END);
+        this._writeDateRange(startDate, endDate);
         break;
-    }
+    }  
+    // Add the new entry to the list of events
+    var newSheetData = {
+      [HEADING['Event']]: eventData[HEADING['Event']],
+      [HEADING['Dept']]: eventData[HEADING['Dept']]
+    };
+    this.sheetData.push(newSheetData)
   };
 
+  // Sorts dates in the proper order & ensures groupings are retained correctly.
+  sortSheet(){
+    var lastColumn = CalendarSheet.getLastColumn();
+    const range = CalendarSheet.getRange(2, 1, CalendarSheet.getLastRow() - 1, lastColumn);
+    range.sort({column: lastColumn, ascending: true});
+  };
+
+// TODO - Try to reorganize this section
   // ========================================
   // ===== Writing and Deleting Entries =====
   // ========================================
 
-  // Writes to the next blank row on to the sheet.
-  _populateRow() {
+  // WRITES the data onto the sheet
+  _writeRow() {
     var lastRow = CalendarSheet.getLastRow();
     var newRow = [];
 
@@ -79,9 +89,9 @@ class SheetEntry {
     const amountRows = 1
     CalendarSheet.getRange(lastRow+1, columnStart, amountRows, columnEnd).setValues([newRow])
     var eventName = this.entryDict[HEADING['Event']]
-    Logger.log("SHEETS. Calendar Page.  Added Entry " + eventName)
   };
 
+// TODO - Try to reorganize this section
   // ========================================
   // ===== Types of Dates to Write =====
   // ========================================
@@ -89,13 +99,13 @@ class SheetEntry {
   const date = new Date(`${START} 1, ${new Date().getFullYear()}`);   // Create Date object.
   this.calendarDict[HEADING['DateRange']] = START
   this.calendarDict[HEADING['Sorting']] = date.getTime() - 1
-  this._populateRow()
-  }
+  this._writeRow()
+  };
 
   _writeSingularDate(START){
     this.calendarDict[HEADING['DateRange']] = START
     this.calendarDict[HEADING['Sorting']] = START.getTime()
-    this._populateRow()
+    this._writeRow()
   };
 
   _writeDateRange(START, END){
@@ -115,76 +125,50 @@ class SheetEntry {
       this.calendarDict[HEADING['EventID']] = this.entryDict[HEADING['Event']]
       this.calendarDict[HEADING['DateRange']] = date
       this.calendarDict[HEADING['Sorting']] = date.getTime()
-      this._populateRow()
+      this._writeRow()
       }
     // Singular entry for date range
     const dateString = createDateString(START, END)
     this.calendarDict[HEADING['EventID']] = ''
     this.calendarDict[HEADING['DateRange']] = dateString
     this.calendarDict[HEADING['Sorting']] = START.getTime() - 1
-    this._populateRow()
+    this._writeRow()
   };
 };
 
 // ###################
 // Helper Functions
 // ###################
-// Creates and returns a dictionary with all calendar headings as keys.
-function createEventDict(entryDict){
-  var eventDict = {}
-  //CalendarSheet.getRange(CalendarPage['headingRange']).getValues()[0]; // get values returns a list of rows
-  const calendarHeadings = CALENDAR_PAGE_RANGE
-  // Fill eventDict with matching values
-  for (var index in calendarHeadings) {
-    var heading = calendarHeadings[index]
-    // Add matching data from entry page to eventDict.
-    if (entryDict.hasOwnProperty(heading)) {
-      eventDict[heading] = entryDict[heading];
-    } else {
-      eventDict[heading] = ''
-    }
-  }
-  return eventDict
-};
-
-// REFACTOR REFERENCES TO BE FLUID INSTEAD OF HARDCODED TODO
-// Get all existing entries
-// Make sure data matches the ones called in event data
+// Get list of all existing entries
+// [{ Event: 'Event', Department: 'Department' }]
 function getSheetData() {
   const headerRow = 1; // Assuming the header row is the first row
   const headers = CalendarSheet.getRange(headerRow, 1, 1, CalendarSheet.getLastColumn()).getValues()[0];
-
-  const dateIndex = headers.indexOf('Tentative Dates');
-  const eventNameIndex = headers.indexOf('Event');
-  const departmentIndex = headers.indexOf('Department');
 
   const dataRange = CalendarSheet.getRange(headerRow + 1, 1, CalendarSheet.getLastRow() - headerRow, CalendarSheet.getLastColumn());
   const eventMatrix = dataRange.getValues();
 
   const eventList = eventMatrix.map(function(row) {
     return {
-      Date: row[dateIndex],
-      Event: row[eventNameIndex],
-      Dept: row[departmentIndex]
+      [HEADING['Event']]: row[headers.indexOf(HEADING['Event'])],
+      [HEADING['Dept']]: row[headers.indexOf(HEADING['Dept'])]
     };
   });
-  return eventList;
-}
 
-// THIS HAS HARD CODING.  Perhaps change the pink data to reference config values? TODO
+  return eventList;
+};
+
+/*
+// Checks for matching Event Name and Department
 function matchEventData(eventData, sheetData) {
   for (const entry of sheetData) {
-    if (eventData.Department === entry.Dept && eventData.Event === entry.Event) {
+    if (eventData.HEADING['Dept'] === entry.HEADING['Dept'] && eventData.HEADING['Event'] === entry.HEADING['Event']) {
       return true; // Match found
     }
   }
   return false; // No match found
-}
-
-// Sorts dates in the proper order & ensures groupings are retained correctly.
-function sortSheet(){
-  var lastColumn = CalendarSheet.getLastColumn();
-  const range = CalendarSheet.getRange(2, 1, CalendarSheet.getLastRow() - 1, lastColumn);
-  range.sort({column: lastColumn, ascending: true});
 };
+*/
 
+// Documentation Notes for Page
+// Duplicates only checks for Event Name & Department.
