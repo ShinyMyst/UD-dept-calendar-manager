e8b20e7cc2332674d2085579e4d0d05819ccae19
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2023-06-21 13:51:52 -0400

    Consistent structure in main.js

--- main.js
+++ main.js
@@ -9,28 +9,30 @@ function main(){
   for (const row of entries) {
     // Pair heading name with corresponding row value
     var curEventData = Object.fromEntries(INPUT_HEADINGS.map((key, i) => [key, row[i]]));
-    console.log(curEventData)
 
-    // Catch Invalid Data Entry on Input Page (RED) TODO
-    if (!input.validEvent(curEventData)){
-      console.log("INVALID")
+  // ============================
+  // ===== Update & Validate ====
+  // ============================
+    if (!input.updateData(curEventData)){
       continue
     }
+    if (!sheet.updateData(curEventData)){
+      continue
+    };
+    if (!gcal.updateData(curEventData)){
+      continue
+    };
 
-    // Catch Duplicates on Calendar Page (or other errors) (GREEN) TODO
-
-    // Catch Google Calendar Errors (BLUE) TODO
-
-    // ERROR COLOR should be added in main but called via an input page function b/c it's on input page
-
-    console.log("VALID")
-    // Create Sheet Entry TODO
-    // Creat Gcal Entry TODO
-    // Delete Row (no increment needed because deleting row) TODO
+    // ==========================
+    // ===== Add Entries =====
+    // ==========================
+    sheet.addEvent()
+    gcal.addEvent()
+    input.deleteRow()
   };
 };
     
 
 // TODO replace all console.logs with better Logger.log messages when done reformatting
-
-// Currently working on sheets page
+// TODO try excepts?
+// TODO better config page
